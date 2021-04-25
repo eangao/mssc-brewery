87fece87f8a5813111ef6301a264cb6f7a95926d
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-04-25 14:53:18 +0800

    adding Mapstruct

--- target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-25T12:57:44+0800",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+    date = "2021-04-25T14:10:08+0800",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
 public class BeerMapperImpl implements BeerMapper {
--- target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/CustomerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/CustomerMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-25T12:57:44+0800",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+    date = "2021-04-25T14:10:08+0800",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
 public class CustomerMapperImpl implements CustomerMapper {
